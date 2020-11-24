8315c2aee05789e4b73da7a56d9640e68fbcb42d
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-24 15:39:19 +0530

    :sparkles: Add v1

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,11 @@
-import { getInput, debug, setFailed, setOutput } from "@actions/core";
-import { getOctokit } from "@actions/github";
+import { getInput, setFailed } from "@actions/core";
 
-const token =
-  getInput("token") || process.env.GH_PAT || process.env.GITHUB_TOKEN;
+const token = getInput("token") || process.env.GH_PAT || process.env.GITHUB_TOKEN;
 
 export const run = async () => {
   if (!token) throw new Error("GitHub token not found");
-  const octokit = getOctokit(token);
 
-  const ms: string = getInput("milliseconds");
-  debug(`Waiting ${ms} milliseconds ...`);
-
-  debug(new Date().toTimeString());
-  await wait(parseInt(ms, 10));
-  debug(new Date().toTimeString());
-
-  setOutput("time", new Date().toTimeString());
-};
-
-export const wait = (milliseconds: number) => {
-  return new Promise<void>((resolve) => setTimeout(() => resolve(), milliseconds));
+  console.log("OK");
 };
 
 run()
