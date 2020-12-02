ac1b1e22e67bb077d3b56ca7bf7e5f8178778b43
Anand Chowdhary <mail@anandchowdhary.com>
2020-12-02 14:15:24 +0530

    :recycle: Read .env from SECRETS_CONTEXT

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { getInput, setFailed } from "@actions/core";
 
 const token = getInput("token") || process.env.GH_PAT || process.env.GITHUB_TOKEN;
+const SECRETS_CONTEXT = process.env.SECRETS_CONTEXT || "{}";
+const allSecrets: Record<string, string> = JSON.parse(SECRETS_CONTEXT);
+Object.keys(allSecrets).forEach((key) => {
+  process.env[key] = allSecrets[key];
+});
 
 export const run = async () => {
   if (!token) throw new Error("GitHub token not found");
